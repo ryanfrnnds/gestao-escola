15adffbc28cfb6b4825f06775825558f6722fca6
Ryan Batista Fernandes <ryan.fernandes@saudece.ce.gov.br>
2021-07-05 16:31:45 -0300

    Adicionando banco em memoria para uso da tarefa no ANGULAR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClasseService } from './core/service/classe/classe.service';
+import { SerieService } from './core/service/serie/serie.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'gestao-escola';
+
+  constructor(private serieService: SerieService, private classeService: ClasseService){}
+
+  ngOnInit() {
+
+    console.log('Alô Brasil!');
+    this.serieService.buscarSeries().subscribe(series => {
+      console.log(series);
+    })
+
+    this.classeService.buscarSeries().subscribe(classes => {
+      console.log(classes);
+    })
+
+  }
+
 }
